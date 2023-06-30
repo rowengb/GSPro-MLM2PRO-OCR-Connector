612a1037fed8b0c85a59b2750971ea1c3d44741e
rowengb <137472076+rowengb@users.noreply.github.com>
2023-06-30 19:59:00 -0400

    Socket Connection Script

---
+++ socket_connection.py
@@ -0,0 +1,7 @@
+import socket
+
+def create_socket_connection(host, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = (host, port)
+    sock.connect(server_address)
+    return sock
